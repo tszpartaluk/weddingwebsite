5df32dac61f7a15c4b1796ac735601f63a55f93a
tszpartaluk <tomasz.szpartaluk@gmail.com>
2024-01-02 02:17:27 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SOURCE_DIR=$SCRIPT_DIR
 REMOTE_DIR="~/public_html"
 REMOTE_HOST=homeserver
-COMMAND="scp $SCRIPT_DIR/* $REMOTE_HOST:$REMOTE_DIR"
+COMMAND="scp $SOURCE_DIR/* $REMOTE_HOST:$REMOTE_DIR"
 
 echo $COMMAND
 $COMMAND
